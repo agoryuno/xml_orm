f771d9e4b963f4a6a5931e2969696804de01a461
agoryuno <43528668+agoryuno@users.noreply.github.com>
2022-01-17 13:09:16 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+setuptools.setup(
+    name='xmlorm',
+    version='0.0.1',
+    author='Alexander Goryunov',
+    author_email='alex.goryunov@gmail.com',
+    description='A basic XML to SQL converter',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/agoryuno/xml_orm',
+    license='MIT',
+    packages=['xmlorm])
